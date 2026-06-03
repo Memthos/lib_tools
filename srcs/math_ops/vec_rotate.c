1cf67a4bee466f303b169fe393575013ba9d7481
Memthos <perrinetrebi.m@gmail.com>
2026-06-03 16:16:39 +0200

    fix: Remove wrong - sign in vec_rotate

--- srcs/math_ops/vec_rotate.c
+++ srcs/math_ops/vec_rotate.c
@@ -6,7 +6,7 @@
 /*   By: mperrine <mperrine@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/03 14:33:47 by mperrine          #+#    #+#             */
-/*   Updated: 2026/06/03 14:38:59 by mperrine         ###   ########.fr       */
+/*   Updated: 2026/06/03 16:16:18 by mperrine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ t_vec3	vec_rotate(t_vec3 v1, t_vec3 axis, double angle)
 	res = vec_add(
 			vec_add(vec_scale(v1, cos_a),
 				vec_scale(vec_cross(axis, v1), sin_a)),
-			vec_scale(axis, vec_dot(axis, v1) * (-1.0 - cos_a)));
+			vec_scale(axis, vec_dot(axis, v1) * (1.0 - cos_a)));
 	if (!isfinite(res.x) || !isfinite(res.y) || !isfinite(res.z))
 	{
 		if (res.x < 0 || res.y < 0 || res.z < 0)
